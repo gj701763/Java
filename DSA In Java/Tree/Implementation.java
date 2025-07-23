4f11a4f5eaebc0b02d6fe214daa99b3ee1dc12ac
gj701763 <gj701763@gmail.com>
2025-07-23 18:02:18 +0530

    adding tree in pre,in and post order

---
+++ DSA In Java/Tree/Implementation.java
@@ -0,0 +1,75 @@
+//package DSA In Java.Tree;
+
+public class Implementation {
+
+    static class  Node {
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }  
+    }
+
+    public static class BinaryTree {
+        static int idx = -1;
+        public Node bulidTree(int node[]) {
+            idx++;
+            if (idx >= node.length || node[idx] == -1) {
+                return null;
+            }
+
+
+            Node newNode = new Node(node[idx]);
+            newNode.left = bulidTree(node);
+            newNode.right = bulidTree(node);
+
+            return newNode;
+        }
+        
+    }
+
+    public static void preorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        System.out.print(root.data+ " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+
+    }
+
+    public static void postorder(Node root) {
+        if(root == null) {
+            return;
+        }
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
+    }
+    public static void main(String[] args) {
+        int node[] = {2,3,4,5,4,-1,-1,3,4,-1,4,5,6,-1,-1,4,6,8};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.bulidTree(node);
+        //System.out.println(root.data);
+        preorder(root);
+        System.out.println();
+        inorder(root);
+        System.out.println();
+        postorder(root);
+        
+    }
+}
--- News.java
+++ News.java
@@ -1,13 +1,71 @@
 import java.util.*;
 
+import java.util.*;
+
 public class News {
-    static int[] nums; 
 
-public static void main(String args[])
+  public static void print(int arr[]) {
+    for(int n : arr) {
+      System.out.print(n +" ");
+    }
+    System.out.println();
+  }
 
-{
+  public static void merge(int arr[], int l, int mid, int r) {
+      int n1 = mid - l + 1;
+        int n2 = r - mid;
+        int left[] = new int[n1];
+        int right[] = new int[n2];
 
-  System.out.println(nums.length);
+        for(int i = 0; i < n1; i++) {
+            left[i] = arr[l+i];
+        }
+        for(int j = 0; j < n2; j++) {
+            right[j] = arr[mid+1+j];
+        }
 
-}
+        int i = 0;
+        int j = 0; 
+        int k = l;
+
+        while (i < n1 && j < n2) {
+          if(left[i] <= right[j]) {
+            arr[k++] = left[i++];
+          }else{
+            arr[k++] = right[j++];
+          }
+        }
+
+        while (i < n1) {
+          arr[k++] = left[i++];
+        }
+
+        while (j < n2) { 
+          arr[k++] = right[j++];
+        }
+
+  }
+
+  public static void mergeSort(int arr[], int l, int r) {
+
+    if(l >= r) return;
+        int mid = l + (r - l) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid+1, r);
+        merge(arr, l, mid, r);
+  }
+    public static void main(String[] args) {
+       try(Scanner sc = new Scanner(System.in)) {
+        System.out.print("Enter yout size is : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter array of element : ");
+        for(int i = 0; i < n; i++) {
+          arr[i] = sc.nextInt();
+        }
+
+        mergeSort(arr, 0, n-1);
+        print(arr);
+       }
+    }
 }
